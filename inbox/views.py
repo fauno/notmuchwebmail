f364a8d081f2b92c890f1f8b98cfcbe15afe9e5e
"fauno ext:(%22) <fauno@kiwwwi.com.ar>
2010-11-04 18:03:22 -0300

    Added README with ideas and license

--- inbox/views.py
+++ inbox/views.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+# notmuch webmail
+# Aiming to be a libre replacement for GMail
+# Copyright (C) 2010 Nicolás Reynolds <fauno@kiwwwi.com.ar>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import notmuch
--- settings.py
+++ settings.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# notmuch webmail
+# Aiming to be a libre replacement for GMail
+# Copyright (C) 2010 Nicolás Reynolds <fauno@kiwwwi.com.ar>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # Django settings for notmuchwebmail project.
 
 DEBUG = True
