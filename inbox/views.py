ea1fdc7bcc15fa6dc8acccf72f0aab4286e3a368
"fauno ext:(%22) <fauno@kiwwwi.com.ar>
2010-11-04 21:35:31 -0300

    Removed HttpResponse

--- inbox/views.py
+++ inbox/views.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import notmuch
 import time
